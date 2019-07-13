674dedf5b58d543d65c934ec89698dcb9d8081e7
Tauhid <thtauhid.71@gmail.com>
2019-07-13 23:16:46 +0600

    result model complete (primary)

---
+++ backend/models/result.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const resultSchema = new Schema({
+    reg: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    bengali: {
+        type: Number,
+        required: true,
+    },
+    english: {
+        type: Number,
+        required: true,
+    },
+    math: {
+        type: Number,
+        required: true,
+    }
+}, {
+    timestamps: true
+})
