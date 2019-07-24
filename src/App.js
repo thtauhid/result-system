02b79362ff91dd16212469bea914d834d251498d
Tauhid <thtauhid.71@gmail.com>
2019-07-25 00:55:56 +0600

    Cleaning up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import ViewResult from './components/ViewResult'
+import AddNewResult from './components/AddNewResult'
+import AddStudent from './components/AddStudent'
+import AskResult from './components/AskResult'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+        <br/>
+        	<Router>
+        		<Route path="/" exact component={AskResult} />
+        		<Route path="/add/student" component={AddStudent} />
+        		<Route path="/add/result" component={AddNewResult} />
+        		<Route path="/view" component={ViewResult} />
+        	</Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
