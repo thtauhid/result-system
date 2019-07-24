ec06fba8a0f469b2ece8c7b60c5141425a2ba8a5
Tauhid <thtauhid.71@gmail.com>
2019-07-25 00:56:17 +0600

    adding list all students option

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 let Student = require('../models/student.model')
 
+// List all student
+router.route('/list').get((req, res) => {
+    Student.find()
+        .then(result => res.json(result))
+        .catch(err => res.staus(400).json('Error: ', +err))
+})
+
+
 // Add New Student
 router.route('/add').post((req, res) => {
     const roll = req.body.roll
@@ -12,6 +20,7 @@ router.route('/add').post((req, res) => {
         year
     })
 
+    // Save to DB
     newStudent.save()
         .then(res.json('Student Added!'))
         .catch(err => res.status(400).json('Error: ', +err))
